c8146c0f845e2fe9e01e6cf6cc865c5983c97203
kovidravi <100708627+kovidravi@users.noreply.github.com>
2022-03-01 21:45:00 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import joblib
+model = joblib.load('spam-ham')
+ip = st.text_input('Enter the Message')
+op = model.predict([ip])
+if st.button('Predict'):
+  st.title(op[0])
